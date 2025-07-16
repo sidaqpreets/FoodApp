1f43a13576c74d35920eab6a47f4f14cbf4d4ad6
Sidaqpreet Singh <sidaqpreets007@gmail.com>
2025-07-17 03:31:51 +0530

    Update index.js

--- foodapp-server/index.js
+++ foodapp-server/index.js
@@ -16,7 +16,7 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gctiqmx.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nt4c5sa.mongodb.net/?retryWrites=true&w=majority`;
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gctiqmx.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true`;
 
